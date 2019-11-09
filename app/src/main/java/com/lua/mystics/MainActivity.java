39d6856dce4d1cc97dd56e6c8633ea86a715eca9
LN-Dark <pedrombcruz47@gmail.com>
2019-11-09 12:44:59 +0000

    Update

--- app/src/main/java/com/lua/mystics/MainActivity.java
+++ app/src/main/java/com/lua/mystics/MainActivity.java
@@ -17,10 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.menu_remoteview, R.id.menu_upayoga, R.id.menu_meditar, R.id.menu_info)
+                R.id.menu_meditar, R.id.menu_upayoga, R.id.menu_remoteview, R.id.menu_info)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
